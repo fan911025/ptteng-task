45b2a4aeb59951b68f2462143ae126b1b73508f3
Administrator <957868055@qq.com>
2017-04-09 09:48:43 +0800

    add

--- js-1/task1.js
+++ js-1/task1.js
@@ -1,6 +1,5 @@
 // 获取div
 var col = document.getElementsByTagName("div");
-
 function set() {
     // 设置三个随机数字
     figureA = parseInt(Math.random() * 9);
@@ -10,47 +9,33 @@ function set() {
     console.log(figureA);
     console.log(figureB);
     console.log(figureC);
-
-
-
     //解决生成相同随机数字问题
     if (figureA == figureB || figureA == figureC || figureB == figureC) {
         //为真重新随机数字      
         set();
     }
-    //为假继续执行下方随机颜色的指令
+    //为假时直接输出给随机数字设置随机颜色
     else {
-        function colors() {
-            // 十六进制随机颜色
-            //  return '#'+Math.floor(Math.random()*0xffffff).toString(16);
-            // }
-            // 设置随机颜色
-            var r = Math.floor(Math.random() * 256);
-            var g = Math.floor(Math.random() * 256);
-            var b = Math.floor(Math.random() * 256);
-            return "rgb(" + r + ',' + g + ',' + b + ")";
-        }
-        //给随机数字的方块设置随机颜色
-            col[figureA].style.backgroundColor = colors();
-            col[figureB].style.backgroundColor = colors();
-            col[figureC].style.backgroundColor = colors();
-        // 禁止变成相同色//
-        if (col[figureA] == col[figureB] || col[figureA] == col[figureC] || col[figureB] == col[figureC]) {
-            //为真重新随机颜色
-            colors();
-        }
-        //为假直接输出随机颜色
-        else {
-            // 给随机数字的格子设置随机颜色
-            col[figureA].style.backgroundColor = colors();
-            col[figureB].style.backgroundColor = colors();
-            col[figureC].style.backgroundColor = colors();
-        }
+        col[figureA].style.backgroundColor = colors();
+        col[figureB].style.backgroundColor = colors();
+        col[figureC].style.backgroundColor = colors();
+    }
+    function colors() {
+        // 十六进制随机颜色
+        //  return '#'+Math.floor(Math.random()*0xffffff).toString(16);
+        // }
+        // 设置随机颜色
+        var r = Math.floor(Math.random() * 256);
+        var g = Math.floor(Math.random() * 256);
+        var b = Math.floor(Math.random() * 256);
+        return "rgb(" + r + ',' + g + ',' + b + ")";
+    }   
+    // 禁止变成相同色//
+    if (col[figureA] == col[figureB] || col[figureA] == col[figureC] || col[figureB] == col[figureC]) {
+        //为真重新随机颜色
+        colors();
     }
 }
-
-
-
 // do {
 //     var figure = parseInt(Math.random() * 9)
 //     var figure_a = parseInt(Math.random() * 9)
