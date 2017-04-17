2210d55eb76fc239a75c110a937bda4c15743e5c
Administrator <957868055@qq.com>
2017-04-17 16:36:24 +0800

    add

--- js-2/mate.js
+++ js-2/mate.js
@@ -1,11 +1,17 @@
 var st = document.getElementById("subtr");//减号按钮元素节点
 var ad = document.getElementById("add");//加号按钮元素节点
 var number = num.value;
+var arra;
+var arr_a;
+var arr_b;
 function assign(){
     var TotalN = Number(number);//将参数转换为数字
     var arr=[];//总数组
     var arrA=[];//杀手数组
     var arrB=[];//水民数组
+    arra = arr;
+    arr_a = arrA;
+    arr_b = arrB;
     switch (num.value){
        case "6":
        case "7":
@@ -39,12 +45,12 @@ var rol1 = document.getElementById("role");
 rol1.innerHTML = x;
 var rol2 =document.getElementById("role2");
 rol2.innerHTML = y;
-alert("杀手数量为:"+ x +","+"水民数量为:"+ y);
+console.log("杀手数量为:"+ x +","+"水民数量为:"+ y);
 //填充数组
 for(var i = 1; i<=TotalN;i++){
     arr.push(i);
 }
-alert("总人数顺序："+ arr);
+console.log("总人数顺序："+ arr);
 
   //洗牌算法，打乱总人数数组顺序
   Array.prototype.shuffle = function() {
@@ -59,39 +65,43 @@ alert("总人数顺序："+ arr);
   }
 
   arr.shuffle();                                                             
-  alert("洗牌后总人数顺序"+arr);
+  console.log("洗牌后总人数顺序"+arr);
   //输出杀手数组，水民数组
   for (i=0;i < x; i++) {
-    arrA.push(arr[i]);
+    arrA.push(arra[i]);
+    arrA[i]="杀手";
   }
- alert("杀手的数组包含"+arrA);
+ console.log("杀手的数组包含:"+arrA);
 
-  for (i=x;i < TotalN;i++) {
-    arrB.push(arr[i]);
+  for (i=0;i <y;i++) {
+    arrB.push(arra[i]);
+    arrB[i]="水民";
   }
-  alert("水民的数组包含"+arrB);
+  console.log("水民的数组包含:"+arrB);
 
   //把杀手数组、水民数组按从小到大降序排列
-  arrA.sort(function(a,b){return a-b;});
-  arrB.sort(function(a,b){return a-b;});
-  alert("杀手数组为："+arrA);
-  alert("水民数组为："+arrB);
+  // arr.sort(function(a,b){return a-b;});
+  // arrA.sort(function(a,b){return a-b;});
+  // arrB.sort(function(a,b){return a-b;});
+  // console.log("总数组为："+arr);
+  // console.log("杀手数组为："+arrA);
+  // console.log("水民数组为："+arrB);
 //避免数据堆积
 document.getElementById("A").innerHTML = "";
 document.getElementById("B").innerHTML = "";
 //创建节点使其在html中显示
 for (i=0;i<x;i++){
     var para = document.createElement("span");//创建span元素节点
-    var node = document.createTextNode(arrA[i]+"号");//创建文本节点+号
+    var node = document.createTextNode("杀手");//创建文本节点+号
     para.appendChild(node);//将文本节点植入span元素
-    document.getElementById("A").appendChild(para);//将span节点植入ID为a的div节点内
+    document.getElementById("A").appendChild(para);//将span节点植入ID为A的div节点内
 }
 for (i=0;i<y;i++){
     var para = document.createElement("span");   //创建span元素节点
 
-    var node = document.createTextNode(arrB[i]+"号");//创建文本节点+号
+    var node = document.createTextNode("水民");//创建文本节点+号
     para.appendChild(node);//将文本节点植入span元素
-    document.getElementById("B").appendChild(para);//将span节点植入ID为a的div节点内
+    document.getElementById("B").appendChild(para);//将span节点植入ID为B的div节点内
 }
 }
 //减法函数
@@ -126,6 +136,32 @@ function change(){
         number = 6;
     }
 }
+//
+function licensing (){
+        if (number < 6 || number > 18) {
+    alert("请重新输入6-18的玩家人数");
+  }
+  else {
+    alert("请确认是否进行了分配")
+    //并转换为字符串保存
+    var arrc = arr_a.concat(arr_b);
+  //     Array.prototype.shuffle = function() {
+  //   var input = this;
+  //   for (var i = input.length - 1; i >= 0; i--) {
+  //     var randomIndex = Math.floor(Math.random() * (i + 1));//获取小于this.length的随机整数
+  //     var itemAtIdex = input[randomIndex];
+  //     input[randomIndex] = input[i];
+  //     input[i] = itemAtIdex;//input[randomIndex]和input[i]交换值
+  //   }
+  //   // return input;
+  // }
+  arrc.shuffle(); 
+    sessionStorage.setItem("rol1",JSON.stringify(arrc));
+    // sessionStorage.setItem("rol2",JSON.stringify(arr_a));
+    // sessionStorage.setItem("rol3",JSON.stringify(arr_b));
+    location.href = "../js-3/js-3.html";
+}
+}
 function click() {
   // 定义加减的点击事件
   ad.onclick=add;
@@ -134,5 +170,6 @@ function click() {
   num.oninput=change;
   // 检测事件
   btn.onclick = assign;
+  btn2.onclick = licensing;
 }
 click();
---
+++ js-3/js-3.js
@@ -0,0 +1,144 @@
+//获取sessionStorage的数据
+var arr = JSON.parse(sessionStorage.getItem("rol1"));
+console.log(arr);
+// var arrA = JSON.parse(sessionStorage.getItem("rol2"));
+// console.log(arrA);
+// var arrB = JSON.parse(sessionStorage.getItem("rol3"));
+// console.log(arrB);
+//声明全局变量
+var lo = document.getElementById("look");
+var sta = document.getElementById("status");
+var btn = document.getElementById("set");
+var part = document.getElementById("part");
+var phrase = document.getElementById("phrase");
+var num = document.getElementById("num");
+var z = 0;
+var f = 1;
+var s = 2;
+var i;
+var j;
+var k;
+var i = 0; i < arr.length;
+set.onclick = function filipCards() {
+    if(btn.innerHTML=="法官日志"){
+            sessionStorage.setItem("rol1",JSON.stringify(arr));
+            alert(arr);
+            location.href = "../js-3/mate.html";
+    }
+    else if (btn.innerHTML == "查看" + f + "号身份") {
+        if (i++ , i == arr.length) {
+            if (arr[i] == "水民") {
+                part.style.display = "block";
+                phrase.style.display = "block";
+                part.innerHTML = "角色：水民";
+                phrase.innerHTML = "词组：瞅你咋地？";
+                btn.innerHTML = "法官日志";
+            }
+            else {
+                part.style.display = "block";
+                phrase.style.display = "block";
+                part.innerHTML = "角色：杀手";
+                phrase.innerHTML = "词组：你瞅啥？";
+                btn.innerHTML = "法官日志";
+            }
+        }
+        else {
+            btn.innerHTML = "隐藏并传递" + s + "号身份";
+            lo.style.display = "none";
+            sta.style.display = "block";
+            filipCards1();
+        }
+    }
+    else {
+        f++;
+        s++;
+        btn.innerHTML = "查看" + f + "号身份";
+        lo.style.display = "none";
+        sta.style.display = "block";
+        part.style.display = "none";
+        phrase.style.display = "none";
+        num.innerHTML = f;
+    }
+}
+function filipCards1() {
+    if (arr[i] == "杀手") {
+        part.style.display = "block";
+        phrase.style.display = "block";
+        part.innerHTML = "角色：杀手";
+        phrase.innerHTML = "词组：你瞅啥？";
+    }
+    else if (arr[i] == "水民") {
+        part.style.display = "block";
+        phrase.style.display = "block";
+        part.innerHTML = "角色：水民";
+        phrase.innerHTML = "词组：瞅你咋地？";
+    }
+}
+// function click() {
+// set.onclick=filipCards;
+// }
+// click();
+// function filipCards(){
+// for (var i=0;i>arr.length;i++) {
+//     for(var k=0;k>arrA.length;k++){
+//         if(arr[i] == arrA[k]){
+//             alert(arrA[k]);
+//     }
+// }
+// // for(var j=0;j>arrB.length;j++){
+// // if(arr[i] == arrB[j]){ 
+// //     alert(arrB[j]);
+// // }
+// // }
+//     }
+// }
+// if(btn.innerHTML=="法官查看"){
+//     alert("好");
+// }
+// else if(z === arr.length){
+//     lo.style.display="none";
+//     sta.style.display="block";
+//     var para = document.createElement("span");//创建span元素节点
+//     var node = document.createTextNode("角色：水民");//创建文本节点+号
+//     para.appendChild(node);//将文本节点植入spab元素
+//     document.getElementById("part").appendChild(para);
+//     var para = document.createElement("span");//创建span元素节点
+//     var node = document.createTextNode("词组：制杖,别看我");//创建文本节点+号
+//     para.appendChild(node);//将文本节点植入spab元素
+//     document.getElementById("phrase").appendChild(para);
+
+// }
+// else if(z++,z=== arr.length){
+
+// }
+// set.onclick = function filipCards() {
+//     for (var i = 0; i < arr.length; i++) {
+//         for (var k = 0; k < arrA.length; k++) {
+//             if (arr[i] == arrA[k]) {
+//     lo.style.display="none";
+//     sta.style.display="block";
+//     part.innerHTML="角色：杀手";
+//     phrase.innerHTML="词组：看你妹啊";
+//             }
+//     else {
+//         for (var j = 0; j < arrB.length; j++) {
+//             if (arr[i] == arrB[j]) {
+//     lo.style.display="none";
+//     sta.style.display="block";
+//     part.innerHTML="角色：水民";
+//     phrase.innerHTML="词组：靠";
+//             }
+//         }
+    //     for (var j = 0; j < arrB.length; j++) {
+    //         if (arr[i] == arrB[j]) {
+    // lo.style.display="none";
+    // sta.style.display="block";
+    // part.innerHTML="角色：水民";
+    // phrase.innerHTML="词组：靠";
+
+//             }
+//         }
+//     }
+// }
+//     }
+// }
